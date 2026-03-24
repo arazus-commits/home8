4a8ac6fe0e397f4ae2269e2592a42be3f00ffbf2
arazus-commits <araz.osmanov@gmail.com>
2026-03-24 21:10:08 +0300

    пуш

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //задача 1
+        int [] massive = new int[]{1,2,3};
+        double [] massive2 = {1.57,7.654,9.986};
+        int [] box = {5,534,-543,542};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //задача 2
+        System.out.println(Arrays.toString(massive));
+        System.out.println(Arrays.toString(massive2));
+        System.out.println(Arrays.toString(box));
+
+        //задача 3
+        for (int i = 2; i < massive.length; i--) {
+            if (i == massive.length - 3) {
+                System.out.println(massive[i]);
+                break;
+            }
+            System.out.print(massive[i] + ", ");
         }
+        for (int i = 2; i < massive2.length; i--) {
+            if (i == massive2.length - 3) {
+                System.out.println(massive2[i]);
+                break;
+            }
+            System.out.print(massive2[i] + ", ");
+        }
+        for (int i = 3; i < box.length; i--) {
+            if (i == box.length - 4) {
+                System.out.println(box[i]);
+                break;
+            }
+            System.out.print(box[i] + ", ");
+        }
+
+        //задача 4
+        for (int i = 0; i < massive.length; i++) {
+            if (massive[i]%2 != 0) {
+                massive[i]+=1;
+            }
+            if (i == massive.length - 1) {
+                break;
+            }
+        }
+        System.out.println(Arrays.toString(massive));
+
     }
 }
